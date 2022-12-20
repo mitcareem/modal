0bf9a02b2221f43fdedae7271ec8821ab89eb0ee
mitcareem <102579201+mitcareem@users.noreply.github.com>
2022-12-20 08:08:39 +0530

    Update strictMode.js

--- object/strictMode.js
+++ object/strictMode.js
@@ -8,3 +8,7 @@ console.log(returnThis())
 
 
 // In strict mode window or global object is undefined
+/*
+Strcit mode prevent you to by mistake putting the things(props) in global object when you want
+to construct an object.
+*/
