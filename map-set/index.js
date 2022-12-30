3f7484254bbf1e9fc3930342339d7feb9588aa21
mitcareem <102579201+mitcareem@users.noreply.github.com>
2022-12-30 08:39:41 +0530

    Update index.js

--- map-set/index.js
+++ map-set/index.js
@@ -1,3 +1,4 @@
 // weak map keys are object
-// weak set is set that doesn't prevent its element from garbage collected
-// same definition for weak map
+// weak set is set/map that doesn't prevent its element from garbage collector
+// weak map are useful for associating data with objects whose life cycle you can't control or don't want to control.
+// same definition for weak set
